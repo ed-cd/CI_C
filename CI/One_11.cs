71e5578863ee3dbfadbff7f76c74766d3a4c3323
Eugene Dmitriev <edmitriev@pnimedia.com>
2016-08-08 17:25:19 -0700

    initial

---
+++ CI/One_11.cs
@@ -0,0 +1,33 @@
+﻿namespace CI
+{
+    public class One_11
+    {
+        public bool X => true;
+
+        public static bool IsRotation(string original, string candidate)
+        {
+            //rotation is cutting a substring from 0 -n and appending it to the end
+            var lastPos = original.Length - 1;
+            var longestSubstring = 0;
+            for (var outerPos = lastPos; outerPos >= 0; outerPos--)
+            {
+                var charTocompare = original[outerPos];
+                var found = false;
+                for (var innerPos = outerPos; innerPos >= 0; innerPos--)
+                {
+                    if (candidate[innerPos] != charTocompare) continue;
+                    found = true;
+                    for (var innerNext = innerPos-1; innerNext >= 0; innerNext--) {
+                        if (candidate[innerNext] == charTocompare) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    break;
+                }
+                
+            }
+            return false;
+        }
+    }
+}
---
+++ CI/One_11_Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CI
+{
+    [TestClass]
+    public class One_11_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var test = new One_11();
+            Assert.AreEqual(test.X, true);
+        }
+    }
+}
