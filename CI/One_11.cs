a343b30934944b59de14ccc4e0dfc10369ae541b
Eugene Dmitriev <edmitriev@pnimedia.com>
2016-08-09 14:16:20 -0700

    String rotation

--- CI/One_11.cs
+++ CI/One_11.cs
@@ -1,4 +1,6 @@
-﻿namespace CI
+﻿using System.Linq;
+
+namespace CI
 {
     public class One_11
     {
@@ -6,28 +8,39 @@
 
         public static bool IsRotation(string original, string candidate)
         {
+            if (original.Length != candidate.Length) return false;
             //rotation is cutting a substring from 0 -n and appending it to the end
             var lastPos = original.Length - 1;
-            var longestSubstring = 0;
-            for (var outerPos = lastPos; outerPos >= 0; outerPos--)
+
+            var innerStartPos = lastPos;
+
+            var outerPos = lastPos;
+            for (var innerPos = innerStartPos;;)
             {
-                var charTocompare = original[outerPos];
-                var found = false;
-                for (var innerPos = outerPos; innerPos >= 0; innerPos--)
+                var charToCompare = original[outerPos];
+                if (candidate[innerPos] != charToCompare)
                 {
-                    if (candidate[innerPos] != charTocompare) continue;
-                    found = true;
-                    for (var innerNext = innerPos-1; innerNext >= 0; innerNext--) {
-                        if (candidate[innerNext] == charTocompare) {
-                            found = true;
-                            break;
-                        }
+                    if (innerPos == 0)
+                    {
+                        return false;
                     }
-                    break;
+                    innerStartPos--;
+                    innerPos = innerStartPos;
+                    outerPos = lastPos;
                 }
-                
+                else
+                {
+                    if (innerPos == 0)
+                    {
+                        goto breakOutOfOuterLoop;
+                    }
+                    outerPos--;
+                    innerPos--;
+                }
             }
-            return false;
+            breakOutOfOuterLoop:
+            var candidateSubstring = candidate.Substring(innerStartPos + 1, candidate.Length - innerStartPos - 1);
+            return !candidateSubstring.Where((val, index) => original[index] != val).Any();
         }
     }
 }
--- CI/One_11_Test.cs
+++ CI/One_11_Test.cs
@@ -9,8 +9,22 @@ namespace CI
         [TestMethod]
         public void TestMethod1()
         {
-            var test = new One_11();
-            Assert.AreEqual(test.X, true);
+            const string original1 = "AABABBA";
+            const string rotation1 = "BABBAAA";
+            const string notRotation1А = "BABBAAB";
+            const string notRotation1Б = "АABBAAB";
+            const string original2 = "abcdefgh";
+            const string rotation2A = "abcdefgh";
+            const string rotation2B = "habcdefg";
+            const string notRotation2A = "abcdefah";
+            const string notRotation2B = "hahcdefg";
+            Assert.AreEqual(One_11.IsRotation(original1, rotation1), true);
+            Assert.AreEqual(One_11.IsRotation(original1, notRotation1А), false);
+            Assert.AreEqual(One_11.IsRotation(original1, notRotation1Б), false);
+            Assert.AreEqual(One_11.IsRotation(original2, rotation2A), true);
+            Assert.AreEqual(One_11.IsRotation(original2, rotation2B), true);
+            Assert.AreEqual(One_11.IsRotation(original2, notRotation2A), false);
+            Assert.AreEqual(One_11.IsRotation(original2, notRotation2B), false);
         }
     }
 }
