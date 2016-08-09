400f48af303d3459d83f013117e470d3561ee16b
Eugene Dmitriev <edmitriev@pnimedia.com>
2016-08-09 16:54:28 -0700

    Towers of Hanoi

---
+++ CI/Three_3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CI
+{
+    class Three_3
+    {
+        public static void solveTowersOfHanoi(Stack<int> A, Stack<int> B, Stack<int> C)
+        {
+            Move(A.Count, A, B, C);
+        }
+
+        private static void Move(int n, Stack<int> source, Stack<int> target, Stack<int> auxilliary)
+        {
+            if (n == 0) return;
+            Move(n - 1, source, auxilliary, target);
+            target.Push(source.Pop());
+            Move(n - 1, auxilliary, target, source);
+        }
+    }
+
+    public static class MyExtensions
+    {
+        public static void Times(this int times, Action action)
+        {
+            for (var n = 0; n < times; n++)
+            {
+                action.Invoke();
+            }
+        }
+
+        public static bool CheckTower(this Stack<int> stack, int expectedCount)
+        {
+            if (expectedCount != stack.Count) return false;
+            var arr = stack.ToArray();
+            for (var n = 0; n < expectedCount - 1; n++)
+            {
+                if (arr[n] >= arr[n + 1]) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ CI/Three_3_Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CI
+{
+    [TestClass]
+    public class Three_3_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] arr = {5, 4, 3, 2, 1};
+            var A = new Stack<int>(arr);
+            var B = new Stack<int>();
+            var C = new Stack<int>();
+            var height = A.Count;
+            Three_3.solveTowersOfHanoi(A, C, B);
+            Assert.AreEqual(C.CheckTower(height), true);
+        }
+    }
+}
