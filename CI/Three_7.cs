303d804b9fbbc994c1ac3ba83359db7b3c1edf9d
Eugene Dmitriev <edmitriev@pnimedia.com>
2016-08-11 16:54:58 -0700

    Up to 3_7 by old

---
+++ CI/Three_7.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace CI
+{
+    public class Shelter
+    {
+        private readonly LinkedList<IAnimal> _animalsInShelter = new LinkedList<IAnimal>();
+        public int Count => _animalsInShelter.Count;        
+
+        public void Enqueue(IAnimal animal)
+        {
+            _animalsInShelter.AddLast(animal);            
+        }
+
+        public IAnimal DequeueAny()
+        {
+            if (_animalsInShelter.Count == 0)
+            {
+                throw new Exception("No animals");
+            }
+            var ret = _animalsInShelter.First.Value;
+            _animalsInShelter.RemoveFirst();
+            return ret;
+        }
+
+        public T Dequeue<T>() where T : class,IAnimal
+        {
+            if (_animalsInShelter.Count == 0) {
+                throw new Exception("No animals");
+            }
+            var node = _animalsInShelter.First;
+            do
+            {
+                var val = node.Value;
+                if (val is T)
+                {
+                    _animalsInShelter.Remove(node);
+                    return val as T;
+                }
+                node = node.Next;
+            } while (node != null);
+            return null;
+        }
+
+        public Cat DequeueCat()
+        {
+            return Dequeue<Cat>();
+        }
+        public Dog DequeueDog() {
+            return Dequeue<Dog>();
+        }
+    }
+
+    public interface IAnimal
+    {
+        string Type { get; }
+    }
+
+    public class Cat : IAnimal
+    {
+        private static int _globalCount = 0;
+        private readonly int _count;
+
+        public Cat()
+        {
+            _globalCount ++;
+            _count = _globalCount;
+        }
+        public string Type => $"Cat {_count}";
+    }
+
+    public class Dog : IAnimal
+    {
+        private static int _globalCount = 0;
+        private readonly int _count;
+
+        public Dog()
+        {
+            _globalCount++;
+            _count = _globalCount;
+        }
+        public string Type => $"Dog {_count}";
+    }
+}
---
+++ CI/Three_7_Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CI
+{
+    [TestClass]
+    public class Three_7_Test
+    {
+        [TestMethod]
+        public void TestMethod1() {
+            var shelter = new Shelter();
+            shelter.Enqueue(new Cat());//
+            shelter.Enqueue(new Cat());//
+            shelter.Enqueue(new Dog());//
+            shelter.Enqueue(new Cat());//
+            shelter.Enqueue(new Cat());//
+            shelter.Enqueue(new Cat());
+            shelter.Enqueue(new Dog());//
+            shelter.Enqueue(new Dog());//
+            shelter.Enqueue(new Dog());
+
+            Assert.AreEqual(shelter.DequeueDog().Type,"Dog 1");//
+            Assert.AreEqual(shelter.DequeueCat().Type, "Cat 1");//
+            Assert.AreEqual(shelter.DequeueAny().Type, "Cat 2");//
+            Assert.AreEqual(shelter.DequeueDog().Type, "Dog 2");//
+            Assert.AreEqual(shelter.DequeueAny().Type, "Cat 3");//
+            Assert.AreEqual(shelter.DequeueAny().Type, "Cat 4");//
+            Assert.AreEqual(shelter.DequeueDog().Type, "Dog 3");//
+            Assert.AreEqual(shelter.DequeueAny().Type, "Cat 5");//
+            Assert.AreEqual(shelter.DequeueAny().Type, "Dog 4");//
+        }
+    }
+}
