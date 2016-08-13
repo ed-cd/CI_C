e4a6acead2edcefede9bfb0042d50578cf052c4b
Eugene Dmitriev <edmitriev@pnimedia.com>
2016-08-12 17:35:07 -0700

    Up to Four_1

---
+++ CI/Four_1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CI
+{
+    [DebuggerDisplay("Value = {Value}")]
+    public class TreeNode<T> where T : IComparable
+    {
+        public T Value { get; set; }
+        public TreeNode<T> Left = null;
+        public TreeNode<T> Right = null;
+
+        public TreeNode(T value)
+        {
+            Value = value;
+        }
+
+        public static bool operator <(TreeNode<T> left, TreeNode<T> right)
+        {
+            return left.Value.CompareTo(right.Value) < 0;
+        }
+
+        public static bool operator >(TreeNode<T> left, TreeNode<T> right)
+        {
+            return !(left < right);
+        }
+    }
+
+    public class Btree<T> where T : IComparable
+    {
+        private TreeNode<T> _root;
+
+        public void Add(T value)
+        {
+            var nodeToAdd = new TreeNode<T>(value);
+            _root = Add(nodeToAdd, _root);
+        }
+
+        private static TreeNode<T> Add(TreeNode<T> nodeToAdd, TreeNode<T> root)
+        {
+            if (root == null)
+            {
+                return nodeToAdd;
+            }
+            if (nodeToAdd < root)
+            {
+                root.Left = Add(nodeToAdd, root.Left);
+            }
+            else
+            {
+                root.Right = Add(nodeToAdd, root.Right);
+            }
+            return root;
+        }
+
+        public bool IsBalanced()
+        {
+            return _isBalanced(_root) != -1;
+        }
+
+        private int _isBalanced(TreeNode<T> root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            var leftDepth = _isBalanced(root.Left);
+            var rightDepth = _isBalanced(root.Right);
+            if (leftDepth == -1 || rightDepth == -1 || Math.Abs(leftDepth - rightDepth) > 1)
+            {
+                return -1;
+            }
+            return Math.Max(leftDepth, rightDepth) + 1;
+        }
+    }
+}
---
+++ CI/Four_1_Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CI
+{
+    [TestClass]
+    public class Four_1_Test
+    {
+        [TestMethod]
+        public void TestMethod1() {
+            var tree = new Btree<int>();
+            tree.Add(1);
+            tree.Add(2);
+            tree.Add(3);
+            tree.Add(0);
+            tree.Add(1);
+            Assert.IsTrue(tree.IsBalanced());
+            tree.Add(1);      
+            Assert.IsFalse(tree.IsBalanced());      
+            var x = 1;
+        }
+    }
+}
