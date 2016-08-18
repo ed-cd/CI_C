09b8e5a283f5f7a5a80ae9acb5a0029663b89fda
Eugene Dmitriev <edmitriev@pnimedia.com>
2016-08-18 13:38:40 -0700

    Tree search using DFS and BFS passed

---
+++ CI/Four_2.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace CI
+{
+    using System;
+
+    public class Graph<N, E>
+    {
+        public readonly List<GraphNode<N, E>> Vertices = new List<GraphNode<N, E>>();
+        public int Count => Vertices.Count;
+        public int NumberOfEdges { get; private set; } = 0;
+
+        public void AddVertex(GraphNode<N, E> vertex)
+        {
+            Vertices.Add(vertex);
+        }
+
+        // ReSharper disable once InconsistentNaming
+        public void AddEdge(GraphNode<N, E> from, GraphNode<N, E> to, E weight, bool isDirected)
+        {
+            if (to == null) throw new ArgumentNullException(nameof(to));
+            if (!Vertices.Contains(from) || !Vertices.Contains(to))
+            {
+                throw new GraphException();
+            }
+            var newEdge = new GraphEdge<N, E>(from, to, weight);
+
+            from.OutgoingEdges.Add(newEdge);
+            to.IncomingEdges.Add(newEdge);
+            NumberOfEdges++;
+            if (isDirected) return;
+            var newEdgeReversed = new GraphEdge<N, E>(to, from, weight);
+            from.IncomingEdges.Add(newEdgeReversed);
+            to.OutgoingEdges.Add(newEdgeReversed);
+            NumberOfEdges++;
+        }
+
+        public bool IsRouteDfs(GraphNode<N, E> from, GraphNode<N, E> to)
+        {
+            return @from == to || IsRouteDfs(@from, to, new Dictionary<GraphNode<N, E>, bool>());
+        }
+
+        private static bool IsRouteDfs(GraphNode<N, E> current, GraphNode<N, E> to,
+            Dictionary<GraphNode<N, E>, bool> visitedNodes)
+        {
+            if (current == to)
+            {
+                return true;
+            }
+            visitedNodes.Add(current, true);
+            foreach (var edge in current.OutgoingEdges)
+            {
+                var nextNode = edge.To;
+                if (visitedNodes.Keys.Contains(nextNode)) continue;
+                if (IsRouteDfs(nextNode, to, visitedNodes))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool IsRouteBfs(GraphNode<N, E> currentNode, GraphNode<N, E> to)
+        {
+            if (currentNode == to)
+            {
+                return true;
+            }
+            var visitedNodes = new Dictionary<GraphNode<N, E>, bool>();
+            var nodesToCheck = new Queue<GraphNode<N, E>>();
+            while (visitedNodes.Count < Count)
+            {
+                visitedNodes[currentNode] = true;
+                foreach (var edge in currentNode.OutgoingEdges)
+                {
+                    var nodeToCheck = edge.To;
+                    if (nodeToCheck == to)
+                    {
+                        return true;
+                    }
+                    if (!visitedNodes.ContainsKey(nodeToCheck))
+                    {
+                        nodesToCheck.Enqueue(nodeToCheck);
+                    }
+                }
+                if (nodesToCheck.Count == 0)
+                {
+                    break;
+                }
+                currentNode = nodesToCheck.Dequeue();
+            }
+
+            return false;
+        }
+    }
+
+    public class GraphException : Exception
+    {
+    }
+
+    [DebuggerDisplay("From {From.Value} to {To.Value}")]
+    public class GraphEdge<N, E>
+    {
+        public GraphNode<N, E> From { get; private set; }
+        public GraphNode<N, E> To { get; private set; }
+
+        public E Weight { get; private set; }
+
+        public GraphEdge(GraphNode<N, E> from, GraphNode<N, E> to, E weight)
+        {
+            if (from == null || to == null || from == to)
+            {
+                throw new GraphException();
+            }
+            From = from;
+            To = to;
+            Weight = weight;
+        }
+    }
+
+    [DebuggerDisplay("{Value}")]
+    public class GraphNode<N, E>
+    {
+        public N Value { get; private set; }
+        public readonly List<GraphEdge<N, E>> OutgoingEdges = new List<GraphEdge<N, E>>();
+        public readonly List<GraphEdge<N, E>> IncomingEdges = new List<GraphEdge<N, E>>();
+
+        protected bool Equals(GraphNode<N, E> other)
+        {
+            return !ReferenceEquals(null, other) && Uuid.Equals(other.Uuid);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == this.GetType() && Equals((GraphNode<N, E>) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Uuid.GetHashCode();
+        }
+
+        public Guid Uuid { get; }
+
+        public GraphNode(N value)
+        {
+            Uuid = Guid.NewGuid();
+            Value = value;
+        }
+
+        public static bool operator ==(GraphNode<N, E> node1, GraphNode<N, E> node2)
+        {
+            return node1.Equals(node2);
+        }
+
+        public static bool operator !=(GraphNode<N, E> node1, GraphNode<N, E> node2)
+        {
+            return !(node1 == node2);
+        }
+    }
+}
---
+++ CI/Four_2_Test.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CI
+{
+    [TestClass]
+    public class Four_2_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var graph = new Graph<int, int>();
+            var node1 = new GraphNode<int, int>(1);
+            var node2 = new GraphNode<int, int>(2);
+            var node3 = new GraphNode<int, int>(3);
+            var node4 = new GraphNode<int, int>(4);
+            var node5 = new GraphNode<int, int>(5);
+            var node6 = new GraphNode<int, int>(6);
+            var node7 = new GraphNode<int, int>(7);
+            var node8 = new GraphNode<int, int>(8);
+            var node9 = new GraphNode<int, int>(9);
+            var node0 = new GraphNode<int, int>(0);
+
+            graph.AddVertex(node1);
+            graph.AddVertex(node2);
+            graph.AddVertex(node3);
+            graph.AddVertex(node4);
+            graph.AddVertex(node5);
+            graph.AddVertex(node6);
+            graph.AddVertex(node7);
+            graph.AddVertex(node8);
+            graph.AddVertex(node9);
+            graph.AddVertex(node0);
+
+            graph.AddEdge(node1, node2, 1, false);
+            graph.AddEdge(node4, node1, 1, true);
+            graph.AddEdge(node3, node4, 1, false);
+            graph.AddEdge(node3, node4, 1, false);
+            graph.AddEdge(node3, node4, 1, false);
+            graph.AddEdge(node3, node5, 1, false);
+            graph.AddEdge(node5, node6, 1, true);
+            graph.AddEdge(node5, node7, 1, false);
+            graph.AddEdge(node7, node8, 1, false);
+            graph.AddEdge(node9, node0, 1, false);
+            graph.AddEdge(node4, node6, 1, true);
+            graph.AddEdge(node5, node4, 1, true);
+
+            Assert.IsTrue(graph.IsRouteDfs(node8, node1));
+            Assert.IsFalse(graph.IsRouteDfs(node1, node8));
+            Assert.IsTrue(graph.IsRouteDfs(node4, node6));
+            Assert.IsFalse(graph.IsRouteDfs(node6, node4));
+            Assert.IsTrue(graph.IsRouteDfs(node3, node8));
+            Assert.IsFalse(graph.IsRouteDfs(node0, node4));
+            Assert.IsTrue(graph.IsRouteDfs(node9, node0));
+
+            Assert.IsTrue(graph.IsRouteBfs(node8, node1));
+            Assert.IsFalse(graph.IsRouteBfs(node1, node8));
+            Assert.IsTrue(graph.IsRouteBfs(node4, node6));
+            Assert.IsFalse(graph.IsRouteBfs(node6, node4));
+            Assert.IsTrue(graph.IsRouteBfs(node3, node8));
+            Assert.IsFalse(graph.IsRouteBfs(node0, node4));
+            Assert.IsTrue(graph.IsRouteBfs(node9, node0));
+        }
+    }
+}
