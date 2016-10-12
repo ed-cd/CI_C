2627cc9d07933c072e0b21054445714da08e878d
Eugene Dmitriev <edmitriev@pnimedia.com>
2016-10-12 16:40:54 -0700

    Doing 7_5 of E5

---
+++ CI/Seven_3_Test.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CI {
+    [TestClass]
+    public class Seven_3_Test {
+        [TestMethod]
+        public void TestMethod1() {
+            Assert.AreEqual(7-3,7.mySubtract(3));
+            Assert.AreEqual(7 - 9, 7.mySubtract(9));
+            Assert.AreEqual(7 + 9, 7.mySubtract(-9));
+            Assert.AreEqual(-7 + 9, 7.mySubtract(9));
+
+            Assert.AreEqual(7 * 3, 7.myMultiply(3));
+            Assert.AreEqual(7 * -3, 7.myMultiply(-3));
+            Assert.AreEqual(-7 * 3, -7.myMultiply(3));
+            Assert.AreEqual(-7 * -3, -7.myMultiply(-3));
+
+            Assert.AreEqual(7 / 3, 7.myDivide(3));
+            Assert.AreEqual(7 / -3, 7.myDivide(-3));
+            Assert.AreEqual(-7 / 3, -7.myDivide(3));
+            Assert.AreEqual(-7 / -3, 7.myDivide(-3));
+        }
+    }
+
+    public static class Seven_3_Extensions {
+        public static int mySubtract(this int n1, int n2) {
+            var twosCompliment = ~n2 + 1;
+            return n1 + twosCompliment;
+        }
+
+        public static int myMultiply(this int n1, int n2) {
+            if (n1 == 0 || n2 == 0) {
+                return 0;
+            }
+            var result = n1;
+            if (n1 > 0) {
+                if (n2 > 0) {
+                    for (var n = 0; n < n2 - 1; n++) {
+                        result += n1;
+                    }
+                } else {
+                    result = 0.mySubtract(n1);
+                    n2 = 0.mySubtract(n2);
+                    for (var n = 0; n < n2 - 1; n++) {
+                        result = result.mySubtract(n1);
+                    }
+                }
+            } else {
+                if (n2 > 0) {
+                    for (var n = 0; n < n2 - 1; n++) {
+                        result = result.mySubtract(n1);
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static int myDivide(this int n1, int n2) {
+            var n = 0;
+            if (n1 > 0) {
+                for (; n1 > 0; n++) {
+                    n1 = n1.mySubtract(n2);
+                }
+            } else {
+                for (; n1 < 0; n++) {
+                    n1 += n2;
+                }
+            }
+            return n;
+        }
+    }
+}
---
+++ CI/Seven_5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+
+namespace CI {
+    [TestClass]
+    public class Seven_5 {
+        [TestMethod]
+        public void TestMethod1() {}
+    }
+
+    public class Square {
+        public Point corner1, corner2, corner3, corner4;
+
+        public Point Center => new Point((corner1.X + corner3.X)/2, (corner1.Y + corner3.Y)/2);
+    }
+}
