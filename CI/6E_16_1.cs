7fd0e52a14a7b28a10e810c044c1594b79864f56
edmitriev <eugene.d.001@gmail.com>
2016-11-03 17:38:53 -0700

    Up to 16_1 6e

---
+++ CI/6E_16_1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CI
+{
+    [TestClass]
+    public class E6_16_1_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var a = 7;
+            var b = 11;
+
+            Swap(ref a, ref b);
+            Assert.AreEqual(a,11);
+            Assert.AreEqual(b, 7);
+            Swap2(ref a,ref b);
+            Assert.AreEqual(a, 7);
+            Assert.AreEqual(b, 11);
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            a = a ^ b;
+            b = b ^ a;
+            a = a ^ b;
+        }
+        private static void Swap2(ref int a, ref int b)
+        {
+            if (b >= a)
+            {
+                b = b - a;
+                a = a + b;
+                b = a - b;
+            }
+            else
+            {
+                a = a - b;
+                b = a + b;
+                a = b - a;
+            }
+        }
+
+    }
+}
