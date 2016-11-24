f07256c369e6815bf68fb3b47701e197ddf4f9be
ed-cd <metatron.addict@gmail.com>
2016-11-23 21:38:18 -0800

    blah

---
+++ CI/_6E_16_2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CI
+{
+    [TestClass]
+    public class _6E_16_2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        int getFrequency(string[] book, string word, ref Dictionary<string,int> hash)
+        {
+            if (!hash.ContainsKey(word.ToLower()))
+            {
+                hash[word.ToLower()] = book.Aggregate(0,
+                    (count, currentWord) => count + currentWord.ToLower() == word.ToLower() ? 1 : 0);
+            }
+            return hash[word.ToLower()];
+        }
+
+
+    }
+}
---
+++ CI/_6E_16_5_Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CI
+{
+    [TestClass]
+    public class _6E_16_5_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        public int findTrailing0z(int nFactorial)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
