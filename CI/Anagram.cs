bc03fda2ef97adfa471e3e190c849124fe8bdb06
ed-cd <metatron.addict@gmail.com>
2017-03-13 21:58:44 -0700

    Started on Anagram

---
+++ CI/Anagram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CI
+{
+    [TestClass]
+    public class Anagram
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Find the minimum number of characters of the first string that we need to change in order to make it an anagram of the second string.
+
+        }
+
+        private static bool IsAnagram(string s1, string s2)//strings will have equal lengths
+        {//The given string will contain only characters from  to .
+            return false;
+        }
+    }
+}
