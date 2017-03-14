4ece4bc52b3cb28a54bcb12c5a2b9557bb6866cc
ed-cd <metatron.addict@gmail.com>
2017-03-13 22:35:33 -0700

    q

--- CI/Anagram.cs
+++ CI/Anagram.cs
@@ -9,12 +9,17 @@ namespace CI
         [TestMethod]
         public void TestMethod1()
         {
+            //aaa-bbb - 3
+            //a-b     - 1       
+            //mn-op   - 2
+            //xyyx    - 0
+            //xaxb bbxx - 1
             // Find the minimum number of characters of the first string that we need to change in order to make it an anagram of the second string.
-
         }
 
-        private static bool IsAnagram(string s1, string s2)//strings will have equal lengths
-        {//The given string will contain only characters from  to .
+        private static bool ToMakeAnagram(string s1, string s2) //strings will have equal lengths
+        {
+//The given string will contain only characters from  to .
             return false;
         }
     }
