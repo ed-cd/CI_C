11bf1d94e2232591cf045a3666aa7816535f0877
edmitriev <eugene.d.001@gmail.com>
2017-03-23 18:30:32 -0700

    Added testing Async

---
+++ CI/TestAsync.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CI
+{
+    [TestClass]
+    public class TestAsync
+    {
+        //https://blogs.msdn.microsoft.com/benwilli/2015/09/10/tasks-are-still-not-threads-and-async-is-not-parallel/
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        private async Task _testBasicAsync(int delay)
+        {
+            var stopWatch = new Stopwatch();
+        }
+    }
+}
