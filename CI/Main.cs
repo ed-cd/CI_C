e3bf856ea4d55b7cd1192d1770008a2e774ea821
edmitriev <eugene.d.001@gmail.com>
2017-04-04 15:00:58 -0700

    Adding Threads

--- CI/Main.cs
+++ CI/Main.cs
@@ -10,22 +10,7 @@ namespace CI
     {
         public static void Main(string[] args)
         {
-            TestAsync.Test5Tasks().RunSynchronously();
-        }
-        public static List<int> fib(int n1, int n2)
-        {
-            if (n1 <= n2)
-            {
-                throw new Exception("not");
-            }
-
-            var list = new List<int> { n1, n2 };
-            while (list.Last() != 0)
-            {
-                list.Add(list[list.Count - 2] - list[list.Count - 1]);
-                if (list.Last() < 0) throw new Exception("not");
-            }
-            return list;
+            TestAsync.Test5Tasks();
         }
     }
     
---
+++ CI/TestThreads.cs
@@ -0,0 +1,7 @@
+﻿namespace CI
+{
+    public class TestThreads
+    {
+        
+    }
+}
