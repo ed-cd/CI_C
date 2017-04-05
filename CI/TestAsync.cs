5e2b396de10a6334601f9355bb92ecb9be5d3904
edmitriev <eugene.d.001@gmail.com>
2017-04-04 17:05:51 -0700

    More Async

--- CI/TestAsync.cs
+++ CI/TestAsync.cs
@@ -7,9 +7,25 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CI
 {
+    public enum AsyncType
+    {
+        // perform some asynchronous task in various ways
+        // await Task.Yield();       // yields immediately
+        // await Task.Delay(0);      // continues synchronously
+        // await Task.Delay(100);     // continues asynchronously on UI thread
+        // await Task.Delay(0).ConfigureAwait(false);    // continues synchronously
+        // await Task.Delay(100).ConfigureAwait(false);   // continues asynchronously on any thread
+        YieldImmediately,
+        ContinueSynchronously,
+        ContinueAsynchronouslyAfter100Ms,
+        ConfigureAwaitTrue,
+        ConfigureAwaitFalse
+    }
+
     [TestClass]
     public class TestAsync
     {
+        public static int counter;
         //https://blogs.msdn.microsoft.com/benwilli/2015/09/10/tasks-are-still-not-threads-and-async-is-not-parallel/
         [TestMethod]
         public void TestMethod1()
@@ -18,100 +34,60 @@ namespace CI
 
         public static async void Test5Tasks(bool configureAwait = true)
         {
-            await Task.WhenAll(TestBasicAsync(1111, "Task 1"), TestBasicAsync(1111, "Task 2"), TestBasicAsync(1111, "Task 3"),
-                TestBasicAsync(1111, "Task 4"), TestBasicAsync(1111, "Task 5")).ConfigureAwait(false);
+            await TestAsyncType(AsyncType.YieldImmediately);
         }
 
-        public static async Task TestBasicAsync(int delay,string name)
+        public static async Task TestBasicAsync(int delay, string name)
         {
             await Task.Delay(0);
             var sw = Stopwatch.StartNew();
             var start = sw.ElapsedMilliseconds;
             var duration = start + delay;
-            while (sw.ElapsedMilliseconds < duration) { }
+            while (sw.ElapsedMilliseconds < duration)
+            {
+            }
             var end = sw.ElapsedMilliseconds;
             var runTime = end - start;
             Console.WriteLine($"Task {name} completed in {runTime} milliseconds");
         }
 
-        Stopwatch sw = new Stopwatch();
-        private async void bttnStart_Click(object sender, [CallerMemberName] string fname = null)
+        private static async Task TestAsyncType(AsyncType x)
         {
-            Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
-            sw.Restart();
-            Task t1 = DoWorkAsync(Colors.Red, 0, 200);
-            Task t2 = DoWorkAsync(Colors.Blue, 1, 300);
-            Task t3 = DoWorkAsync(Colors.Green, 2, 500);
-            await Task.WhenAll(t1, t2, t3);
-            sw.Stop();
-            txtStatus.Text = $"Finished after {sw.ElapsedMilliseconds}";
+            var t1 = DoWorkAsync(1, 200, x);
+            var t2 = DoWorkAsync(2, 200, x);
+            var t3 = DoWorkAsync(3, 200, x);
+            var t4 = DoWorkAsync(4, 200, x);
+            var t5 = DoWorkAsync(5, 200, x);
+            await Task.WhenAll(t1, t2, t3, t4, t5);
+            Console.WriteLine($"All finished");
         }
 
-        private void bttnClear_Click(object sender, RoutedEventArgs e)
+        private static async Task DoWorkAsync(int taskNum, int delay, AsyncType type)
         {
-            theRootCanvas.Children.Clear();
-        }
-
-        private async Task DoWorkAsync(int taskNum, int delay)
-        {
-            for (var i = 0; i < 5; i++)
+            Console.WriteLine($"Task {taskNum} started");
+            switch (type)
             {
-                // perform some asynchronous task in various ways
-                // await Task.Yield();       // yields immediately
-                // await Task.Delay(0);      // continues synchronously
-                // await Task.Delay(100);     // continues asynchronously on UI thread
-                // await Task.Delay(0).ConfigureAwait(false);    // continues synchronously
-                // await Task.Delay(100).ConfigureAwait(false);   // continues asynchronously on any thread
-
-                var startTime = sw.ElapsedMilliseconds;
-
-                // spin here for 'delay' milliseconds just to keep busy
-                while (sw.ElapsedMilliseconds < startTime + delay) { /* do nothing */ }
-
-                var endTime = sw.ElapsedMilliseconds;
-
-                Dispatcher.BeginInvoke((Action)(() => AddTimeRectangle(
-                                                startTime, endTime, color, taskNum)));
+                case AsyncType.YieldImmediately:
+                    await Task.Yield();
+                    break;
+                case AsyncType.ContinueSynchronously:
+                    await Task.Delay(0);
+                    break;
+                case AsyncType.ContinueAsynchronouslyAfter100Ms:
+                    await Task.Delay(100);
+                    break;
+                case AsyncType.ConfigureAwaitTrue:
+                    await Task.Delay(0).ConfigureAwait(false);
+                    break;
+                case AsyncType.ConfigureAwaitFalse:
+                    await Task.Delay(100).ConfigureAwait(false);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(type), type, null);
             }
+            (delay*1000).nTimes(() => ++counter);
+
+            Console.WriteLine($"Task {taskNum} finished");
         }
-
-        /// <summary>
-        /// add a rectangle that whose height & position represents the start and end time
-        /// of an activity
-        /// </summary>
-        /// <param name="startTime"></param>
-        /// <param name="endTime"></param>
-        /// <param name="color"></param>
-        /// <param name="column"></param>
-        private void AddTimeRectangle(double startTime, double endTime, Color color, int column)
-        {
-            const int columnWidth = 50;
-            Debug.WriteLine("Adding Rect {0} {1} {2}", startTime, endTime, color);
-
-            double left = column * (columnWidth * 2 / 3);
-            double height = endTime - startTime;
-
-            Rectangle r = new Rectangle()
-            {
-                Fill = new SolidColorBrush(color),
-                Width = columnWidth,
-                Height = height,
-            };
-            Canvas.SetLeft(r, left);
-            Canvas.SetTop(r, startTime);
-
-            theRootCanvas.Children.Add(r);
-
-            if (theRootCanvas.Height < endTime)
-            {
-                theRootCanvas.Height = endTime;
-            }
-            if (theRootCanvas.Width < left + columnWidth)
-            {
-                theRootCanvas.Width = left + columnWidth;
-            }
-        }
-
-
     }
 }
