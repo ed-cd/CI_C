4641374468ce437fb3d0c1a0b2cf5818a56267a3
ed-cd <metatron.addict@gmail.com>
2017-04-06 22:01:43 -0700

    Dining Philosophers

--- CI/TestThreads.cs
+++ CI/TestThreads.cs
@@ -1,16 +1,64 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace CI
 {
     public class DP
     {
-        
-    }
+        private CS cs1 = new CS();
+        private CS cs2 = new CS();
+        private CS cs3 = new CS();
+        private CS cs4 = new CS();
+        private CS cs5 = new CS();
 
-    public class CS { }
+        private P p1;
+        private P p2;
+        private P p3;
+        private P p4;
+        private P p5;
+
+        public DP()
+        {
+            p1 = new P(cs1,cs5);
+            p2 = new P(cs2,cs1);
+            p3 = new P(cs3,cs2);
+            p4 = new P(cs4,cs3);
+            p5 = new P(cs5,cs4);
+        }
+
+    }
 
     public class P
     {
-        private int count;
+        public P(CS left, CS right)
+        {
+            Left = left;
+            Right = right;
+        }
+        private int count = 5;
+        public CS Left { get; set; }
+        public CS Right { get; set; }
+
+        public void Eat()
+        {
+        }
+
+        private void PickUpL()
+        {
+        }
+    }
+
+    public class CS
+    {
+        public bool PickUp()
+        {
+            return Monitor.TryEnter(this);
+        }
+
+        public void PutDown()
+        {
+            Monitor.Exit(this);
+        }
     }
 }
